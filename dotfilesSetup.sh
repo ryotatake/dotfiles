330207fe36a05875bd29efc2992b728c3514d1bc
Ryota Takeuchi <takeuchi.ryota@live-revolution.co.jp>
2019-03-18 18:25:22 +0900

    add .bashrc

--- dotfilesSetup.sh
+++ dotfilesSetup.sh
@@ -4,6 +4,7 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/colors ~/.vim
 ln -sf ~/dotfiles/indent ~/.vim
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
 
 #プラグイン管理のためのVundle導入
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
