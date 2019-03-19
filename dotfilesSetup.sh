f1516e150cd482a566a41c5b6fec7825135ebfa0
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2019-03-19 17:46:37 +0900

    add gitconfig

--- dotfilesSetup.sh
+++ dotfilesSetup.sh
@@ -5,6 +5,7 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/colors ~/.vim/colors
 ln -sf ~/dotfiles/indent ~/.vim/indent
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
 #プラグイン管理のためのVundle導入
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
