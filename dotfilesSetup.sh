f28ce37ef19eda1ce6d3c1ceecce45daa3e025dc
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2019-04-05 11:31:43 +0900

    add .tmux.conf

--- dotfilesSetup.sh
+++ dotfilesSetup.sh
@@ -6,6 +6,7 @@ ln -sf ~/dotfiles/colors ~/.vim/colors
 ln -sf ~/dotfiles/indent ~/.vim/indent
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 #プラグイン管理のためのVundle導入
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
