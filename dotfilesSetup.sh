c108dd966d2e387f6e76ade5a2bae02637ef7e75
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2019-08-28 16:39:22 +0900

    update dotfilesSetup

--- dotfilesSetup.sh
+++ dotfilesSetup.sh
@@ -8,6 +8,7 @@ ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.irbrc ~/.irbrc
 
 #プラグイン管理のためのVundle導入
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
