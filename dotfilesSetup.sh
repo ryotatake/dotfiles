99a275197b3e06ed6e2140d455631cd90beff579
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2019-11-30 11:02:55 +0900

    .vimにシンボリックリンクをはる

--- dotfilesSetup.sh
+++ dotfilesSetup.sh
@@ -2,6 +2,7 @@
 
 #シンボリックリンクの作成
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
