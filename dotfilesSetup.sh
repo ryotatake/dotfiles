2d33e6e46493852d2c330c85477b921c7e7c3fc2
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2019-11-30 10:45:46 +0900

    snippet.vimはもう使わなくなっていた。

--- dotfilesSetup.sh
+++ dotfilesSetup.sh
@@ -3,7 +3,6 @@
 #シンボリックリンクの作成
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/colors ~/.vim/colors
-ln -sf ~/dotfiles/indent ~/.vim/indent
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
