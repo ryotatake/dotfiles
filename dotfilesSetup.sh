05e377e84fc8f2ca456603fe0a38030cc3491271
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2019-12-01 20:10:06 +0900

    dein.vimの導入は.vimrcで行うので、こちらは不要になった。

--- dotfilesSetup.sh
+++ dotfilesSetup.sh
@@ -9,13 +9,6 @@ ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.irbrc ~/.irbrc
 
-#プラグイン管理のためのdein.vim導入
-if [ ! -d ~/.cache/dein/repos/github.com/Shougo/dein.vim ]; then
-  curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-  # For example, we just use `~/.cache/dein` as installation directory
-  sh ./installer.sh ~/.cache/dein
-fi
-
 #tmuxのプラグイン管理
 if [ ! -d ~/.tmux/plugins/tpm ]; then
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
