e0e261a81515a395bbfb4e056de320b467d361d7
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2019-12-08 00:20:57 +0900

    setupファイルにinputrcの設定を追加

--- dotfilesSetup.sh
+++ dotfilesSetup.sh
@@ -8,6 +8,7 @@ ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.irbrc ~/.irbrc
+ln -sf ~/dotfiles/.inputrc ~/.inputrc
 
 #tmuxのプラグイン管理
 if [[ ! -d ~/.tmux/plugins/tpm ]]; then
