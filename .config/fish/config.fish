2821b7c2a9eb19199d0e088e0753b5404a98fe49
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2019-12-10 10:10:21 +0900

    fishの設定を追加

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,21 @@
+set -x EDITOR vim
 fish_vi_key_bindings
+
+set -x FZF_DEFAULT_OPTS "--height 40% --layout=reverse --border --inline-info --preview 'head -100 {}'"
 set -x FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -g ""'
+
+set -x PATH /usr/local/bin $HOME/bin /sbin /usr/sbin /usr/bin $PATH
+set -x PATH $HOME/.rbenv/bin $PATH
+
+#if [ "(uname)" = 'Darwin' ]
+#  alias ls="ls -G"
+#else
+#  alias ls="ls --color=auto"
+#end
+
+alias la="ls -al"
+alias ll="ls -l"
+alias rm="rm -i"
+alias cp="cp -i"
+alias mv="mv -i"
+alias g="git"
