957ad6c9d5238883155d3e1acb656fbf0f545b27
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2019-12-13 10:57:40 +0900

    fishでrubyが動かなかったので、fishへの設定を追加する。ついでにbash_profileの記述も微妙だったので修正

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,7 +5,11 @@ set -x FZF_DEFAULT_OPTS "--height 40% --layout=reverse --border --inline-info --
 set -x FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -g ""'
 
 set -x PATH /usr/local/bin $HOME/bin /sbin /usr/sbin /usr/bin $PATH
+
 set -x PATH $HOME/.rbenv/bin $PATH
+if which rbenv > /dev/null
+  status --is-interactive; and source (rbenv init -|psub)
+end
 
 #if [ "(uname)" = 'Darwin' ]
 #  alias ls="ls -G"
