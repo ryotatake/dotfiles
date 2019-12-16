76e6bf9c087ef95ecdb6fc90cf97fe0883747614
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2019-12-16 10:57:47 +0900

    ローカルの設定を.localに移す

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,7 @@
+if [ -f ~/.config/fish/config.fish.local ]
+  source ~/.config/fish/config.fish.local
+end
+
 source ~/.config/fish/aliases.fish
 
 if status --is-interactive
--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -6,9 +6,6 @@ set -x FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -g ""'
 set -x PATH /usr/local/bin $HOME/bin /sbin /usr/sbin /usr/bin $PATH
 
 set -x PATH $HOME/.rbenv/bin $PATH
-# FIXME: local settings; move to config.fish.local
-set -x PATH /usr/local/lib/rbenv/bin $PATH
-set -x RBENV_ROOT /usr/local/lib/rbenv
 if which rbenv > /dev/null
   status --is-interactive; and source (rbenv init -|psub)
 end
