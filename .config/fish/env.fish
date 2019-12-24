ed38f875e4c8c3d2e41f9b8bcd154a73111c21e9
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2019-12-24 13:31:30 +0900

    GO用の設定を追加

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -2,8 +2,13 @@ set -x EDITOR vim
 
 set -x FZF_DEFAULT_OPTS "--height 40% --layout=reverse --border --inline-info --preview 'head -100 {}'"
 set -x FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -g ""'
+set -x GOPATH $HOME/go
 
 set -x PATH /usr/local/bin $HOME/bin /sbin /usr/sbin /usr/bin $PATH
+
+if test -d $GOPATH
+  set -x PATH $GOPATH/bin $PATH
+end
 
 if test -d $HOME/.rbenv/bin
   set -x PATH $HOME/.rbenv/bin $PATH
