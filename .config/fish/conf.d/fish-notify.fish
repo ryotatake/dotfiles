6e642673532bac5621d29cf8fdbad467818ceef9
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2020-01-29 18:10:17 +0900

    長いコマンドを実行した際に通知を送れるようにする

---
+++ .config/fish/conf.d/fish-notify.fish
@@ -0,0 +1,14 @@
+function fish_notify --on-event fish_prompt
+  set _display_status $status
+  if test $CMD_DURATION
+    set secs (math "$CMD_DURATION / 1000")
+    # 10秒以上掛かったら通知する
+    if test $secs -ge 3
+      if which osascript > /dev/null ^&1
+        echo "display notification \"Returned $status, took $secs seconds\" with title \"$history[1]\"" | osascript
+      else
+        echo "LONGRUN COMMAND: $history[1], took $secs secs"
+      end
+    end
+  end
+end
