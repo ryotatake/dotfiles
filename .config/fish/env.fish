a171145f37f8cc4ae50e2217c262c7ecb92f868d
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2020-03-10 09:26:34 +0900

    fzfのプレビューにbatによるシンタックスハイライトを使う

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -1,6 +1,6 @@
 set -x EDITOR vim
 
-set -x FZF_DEFAULT_OPTS "--height 40% --layout=reverse --border --inline-info --preview 'head -100 {}'"
+set -x FZF_DEFAULT_OPTS "--height 40% --layout=reverse --border --inline-info --preview 'bat --style=numbers --color=always {} | head -100'"
 set -x FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -g ""'
 
 set -x PATH /usr/local/bin $HOME/bin /sbin /usr/sbin /usr/bin $PATH
