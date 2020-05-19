9e516c638f18dc6e1451bde53a0eda2aa3a6084e
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2020-05-19 20:17:46 +0900

    vimと同じくjkでノーマルモードに移るようにする

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,6 @@
 function fish_user_key_bindings
   fzf_key_bindings
 
-  # Ctrl-Kでノーマルモードへ
-  bind -M insert -m default \ck force-repaint
+  # jkでノーマルモードへ
+  bind -M insert -m default jk force-repaint
 end
