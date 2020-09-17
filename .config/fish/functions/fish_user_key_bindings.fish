fa336acb3f354436de605b643fc401b13e8b98c1
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2020-09-17 12:11:05 +0900

    fishのキーバインドもdfにする

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -2,6 +2,6 @@ function fish_user_key_bindings
   fzf_key_bindings
   fish_vi_key_bindings --no-erase
 
-  # jkでノーマルモードへ
-  bind -M insert -m default jk force-repaint
+  # dfでノーマルモードへ
+  bind -M insert -m default df force-repaint
 end
