f8aee39aad74d65078ffbdf3c4c1e7af5a9c0141
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2021-03-22 16:15:32 +0900

    abbrを追加

--- .config/fish/abbr.fish
+++ .config/fish/abbr.fish
@@ -4,12 +4,19 @@ abbr --add --global la    "ls -lah --time-style=long-iso"
 abbr --add --global rm    "rm -i"
 abbr --add --global cp    "cp -i"
 abbr --add --global mv    "mv -i"
+
 abbr --add --global g     "git"
 abbr --add --global gs    "git status"
 abbr --add --global ga    "git add"
 abbr --add --global gap   "git add --patch"
 abbr --add --global gc    "git commit --verbose"
-abbr --add --global gca   "git commit --verbose --amend"
+abbr --add --global gca   "git commit --verbose --all"
+abbr --add --global gd    "git diff"
+abbr --add --global gl    "git log"
+abbr --add --global gb    "git branch"
+abbr --add --global gco   "git checkout"
+abbr --add --global gcof  "git branch | fzf | xargs git checkout"
+
 abbr --add --global agl   'ag --pager="less -R"'
 abbr --add --global mux   'sh ~/.local/tmux_setup.sh'
 abbr --add --global memos 'memosearch'
