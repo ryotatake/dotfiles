c74e261a2b84ebb7ff06ad65998a036169267222
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2022-01-18 15:29:00 +0900

    vscode: cdでディレクトリを移動できていなかったのでwgetの方で保存先ディレクトリを指定する

--- fish_setup_for_amazon_linux_2.sh
+++ fish_setup_for_amazon_linux_2.sh
@@ -2,8 +2,6 @@
 set -eux
 
 # `sudo amazon-linux-extras install epel -y`を利用すると最新のfishが利用できないのでCentOS用のものを利用する
-sudo cd /etc/yum.repos.d/
-sudo wget https://download.opensuse.org/repositories/shells:fish:release:3/CentOS_7/shells:fish:release:3.repo
+sudo wget -P /etc/yum.repos.d/ https://download.opensuse.org/repositories/shells:fish:release:3/CentOS_7/shells:fish:release:3.repo
 sudo yum install fish util-linux-user -y
 sudo chsh -s `which fish`
-fish
