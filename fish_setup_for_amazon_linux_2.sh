3265ba81a6e79c9dc4f5697fb3acc79d7d02d511
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2022-01-20 12:34:36 +0900

    vscode: fisherプラグインのインストールを行う

--- fish_setup_for_amazon_linux_2.sh
+++ fish_setup_for_amazon_linux_2.sh
@@ -4,4 +4,6 @@ set -eux
 # `sudo amazon-linux-extras install epel -y`を利用すると最新のfishが利用できないのでCentOS用のものを利用する
 sudo wget -P /etc/yum.repos.d/ https://download.opensuse.org/repositories/shells:fish:release:3/CentOS_7/shells:fish:release:3.repo
 sudo yum install fish util-linux-user -y
+curl -sL https://git.io/fisher | source
+fisher update
 sudo chsh -s `which fish`
