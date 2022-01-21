277793a329cce10c020dd4764d3f4ae0acc60adf
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2022-01-21 12:21:22 +0900

    vscode: -yが漏れていた

--- vscode.remote_container.dotfiles.sh
+++ vscode.remote_container.dotfiles.sh
@@ -1,7 +1,7 @@
 make symlinks
 ./fish_setup_for_amazon_linux_2.sh
 git clone https://scm.l-r.tech/takeuchi/dotfiles-local.git ~/dotfiles-local
-sudo yum install the_silver_searcher
+sudo yum install the_silver_searcher -y
 
 cd ~/tmp
 wget -P ~/tmp https://github.com/sharkdp/bat/releases/download/v0.19.0/bat-v0.19.0-x86_64-unknown-linux-gnu.tar.gz
