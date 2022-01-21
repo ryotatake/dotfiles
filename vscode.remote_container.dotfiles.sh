81303525294bc672b553d4cd57b25bb1151b2495
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2022-01-21 13:35:08 +0900

    vscode: vscode-fzf-quick-openを利用するためにripgrepを入れる

--- vscode.remote_container.dotfiles.sh
+++ vscode.remote_container.dotfiles.sh
@@ -8,3 +8,8 @@ wget -P ~/tmp https://github.com/sharkdp/bat/releases/download/v0.19.0/bat-v0.19
 tar xzvf ~/tmp/bat-v0.19.0-x86_64-unknown-linux-gnu.tar.gz
 mv ~/tmp/bat-v0.19.0-x86_64-unknown-linux-gnu/bat ~/bin/bat
 rm -rf ~/tmp/bat-v0.19.0-x86_64-unknown-linux-gnu.tar.gz ~/tmp/bat-v0.19.0-x86_64-unknown-linux-gnu
+
+wget -P ~/tmp https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz
+tar xzvf ~/tmp/ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz
+mv ~/tmp/ripgrep-13.0.0-x86_64-unknown-linux-musl/rg ~/bin/rg
+rm -rf ~/tmp/ripgrep-13.0.0-x86_64-unknown-linux-musl*
