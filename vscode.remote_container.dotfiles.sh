b54a8655bed1c47aceebcf9f598b4f83eb587a0b
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2022-03-16 09:28:16 +0900

    vscode: ER図作成補助のツールを入れる

--- vscode.remote_container.dotfiles.sh
+++ vscode.remote_container.dotfiles.sh
@@ -1,6 +1,11 @@
 make symlinks
 ./fish_setup_for_amazon_linux_2.sh
 git clone https://scm.l-r.tech/takeuchi/dotfiles-local.git ~/dotfiles-local
+
+git clone https://scm.l-r.tech/takeuchi/lrms-update-er-template.git ~/lrms-update-er-template
+cd ~/lrms-update-er-template
+sudo make install
+
 sudo yum install the_silver_searcher -y
 
 cd ~/tmp
