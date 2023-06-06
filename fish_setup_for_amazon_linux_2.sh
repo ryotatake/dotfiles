20781961f8391d23abe7601931d05fa3713df13a
Ryota Takeuchi <ryouta.takeuti@gmail.com>
2023-06-06 13:51:54 +0900

    vscode: dev containerの中ではfishは使わなくて良い
    ターミナルで作業すること自体減らしていきたいので。

--- fish_setup_for_amazon_linux_2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -eux
-
-# `sudo amazon-linux-extras install epel -y`を利用すると最新のfishが利用できないのでCentOS用のものを利用する
-sudo wget -P /etc/yum.repos.d/ https://download.opensuse.org/repositories/shells:fish:release:3/CentOS_7/shells:fish:release:3.repo
-sudo yum install fish -y
-curl -sL https://git.io/fisher | fish -c 'source && fisher update'
--- vscode.remote_container.dotfiles.sh
+++ vscode.remote_container.dotfiles.sh
@@ -1,5 +1,4 @@
 make symlinks
-./fish_setup_for_amazon_linux_2.sh
 git clone https://scm.l-r.tech/takeuchi/dotfiles-local.git ~/dotfiles-local
 
 git clone https://scm.l-r.tech/takeuchi/lrms-update-er-template.git ~/lrms-update-er-template
